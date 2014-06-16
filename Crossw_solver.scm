79e7359a5e0a17485fa673691c7668fe4a4dd0fc
José Castro <JoseCastro@Hydra.(none)>
2014-06-16 18:03:35 -0400

    lee puzzle.txt y words.txt, busca row,col

--- Crossw_solver.scm
+++ Crossw_solver.scm
@@ -1,13 +1,29 @@
 #lang scheme
-
+;Lee puzzle.txt y lo pasa a lista
 (define puzzle (call-with-input-file "crossword_X.txt" read))
 
-(define findInRow
-  (lambda (n lista (s 0)(r '()))
+;Lee words.txt y lo pasa a lista
+(define getWords 
+                (lambda ((list '()) (file (call-with-input-file "words_X.txt" read)))
+                  (cond
+                    ((empty? file) list)
+                    (else (getWords (cons (string->list (car file)) list) (cdr file)))
+                    )
+                  )
+  )
+(define words (getWords))
+(display words)
+(newline)
+;Encuentra elemento de la lista en la posicion X 
+(define getInList
+  (lambda (list x (cx 0))
     (cond
-      ((empty? lista) (reverse r) )
-      ((eq? n (car lista)) (begin (set! r (cons s r)) (findInRow n (cdr lista) (+ s 1) r )))
-      (else (findInRow n (cdr lista) (+ s 1) r )))))
-
-(findInRow '- (car puzzle))
-(findInRow '- (car (cdr puzzle)))
+      ((empty? list) #f)
+      ((eq? list #f) #f)
+      ((= cx x)  (car list))
+      (else (getInList (cdr list) x (+ cx 1)))
+     )
+    )
+  )
+; like get(row,col)=>(y,x)=>(0,9)
+(getInList (getInList puzzle 0) 9)
