0536169bab3e33a87fa628229460daa3089678f0
José Castro <JoseCastro@Hydra.(none)>
2014-06-17 15:40:50 -0400

    ahora si

--- Crossw_solver.scm
+++ Crossw_solver.scm
@@ -40,3 +40,26 @@
 (newline)
 (getByRC puzzle 0 10)
 (getByRC words 1 3)
+;Reeplaza un elemto en list con posicion x
+(define replaceInList
+  (lambda (list x c (cx 0) (nl '()))
+    (cond
+      ((empty? list) (reverse nl))
+      ((eq? x cx) (begin (set! nl (cons c nl)) (replaceInList (cdr list) x c (+ cx 1) nl)))
+      (else (begin (set! nl(cons (car list) nl)) (replaceInList (cdr list) x c (+ cx 1) nl)))
+      )
+    )
+  )
+;Reeplaza un elemento en pzzl en la pos row,col
+(define replaceByRC
+  (lambda (pzzl y x c (cy 0) (np '()))
+    (cond
+      ((empty? pzzl) (reverse np))
+      ((eq? y cy) (begin (set! np (cons (replaceInList (car pzzl) x c) np)) (replaceByRC (cdr pzzl) y x c (+ cy 1) np)))
+      (else (begin (set! np (cons (car pzzl) np)) (replaceByRC (cdr pzzl) y x c (+ cy 1) np)))
+      )
+    )
+  )
+
+;(replaceInList (list 1 2 3 4 5) 0 'X )
+(replaceByRC puzzle 5 0 'X)
