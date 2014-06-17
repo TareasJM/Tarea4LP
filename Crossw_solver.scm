acc1edda6c694c27b04067f3b938d15662ab3523
José Castro <JoseCastro@Hydra.(none)>
2014-06-17 15:39:00 -0400

    replace in pzzl

--- Crossw_solver.scm
+++ Crossw_solver.scm
@@ -3,15 +3,20 @@
 (define puzzle (call-with-input-file "crossword_X.txt" read))
 
 ;Lee words.txt y lo pasa a lista
-(define getWords
-  (lambda ((list '()) (file (call-with-input-file "words_X.txt" read)))
+(define words
+ (letrec
+  ((gw
+   (lambda ((list '()) (file (call-with-input-file "words_X.txt" read)))
     (cond
       ((empty? file) list)
-      (else (getWords (cons (string->list (car file)) list) (cdr file)))
+      (else (gw (cons (string->list (car file)) list) (cdr file)))
       )
     )
-  )
-(define words (getWords))
+  ))
+  (gw)
+ )
+)
+
 ;Encuentra elemento de la lista en la posicion X 
 (define getInList
   (lambda (list x (cx 0))
@@ -26,8 +31,12 @@
 ;Encuentra elemento en pzzl en fila row y columna col
 (define getByRC
   (lambda (pzzl y x)
-    (getInList (getInList puzzle y) x)
+    (getInList (getInList pzzl y) x)
     )
   )
-
+(display puzzle)
+(newline)
+(display words)
+(newline)
 (getByRC puzzle 0 10)
+(getByRC words 1 3)
