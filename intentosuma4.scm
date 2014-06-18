da4f398e281bc31cec525a38991e257f702c5bb8
Jose <n3ggro@gmail.com>
2014-06-18 11:46:59 -0400

    working dir cleaned

--- intentosuma4.scm
+++ intentosuma4.scm
@@ -1,13 +1,19 @@
 #lang scheme
+(define numbers '(1 2 3))
 
-(define puzzle (call-with-input-file "crossword_X.txt" read))
-
+(define suma
+  (lambda ((s 0) (n numbers) (a '()) (c '()))
+    (cond
+      ((empty? n) a)
+      ((= s 4)(begin (set! a (cons c a))) a )
+      ((> s 4) a )
+      (else )
+      )
+    )
+  )
 (define findInRow
   (lambda (n lista (s 0)(r '()))
     (cond
       ((empty? lista) (reverse r) )
       ((eq? n (car lista)) (begin (set! r (cons s r)) (findInRow n (cdr lista) (+ s 1) r )))
       (else (findInRow n (cdr lista) (+ s 1) r )))))
-
-(findInRow '- (car puzzle))
-(findInRow '- (car (cdr puzzle)))
